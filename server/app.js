52f06ac3fc365978b6f175643a1b738bcad42cf7
SaadNiaziDev <saad.niaxi47@gmail.com>
2022-06-04 13:35:10 +0500

    EACCES fix

--- server/app.js
+++ server/app.js
@@ -28,6 +28,6 @@ app.use(cors());
 app.use(authRoute);
 app.use(httpResponse.Middleware)
 
-app.listen(process.env.PORT, () => {
-  console.log("listening on port " + process.env.PORT);
+app.listen(3001, () => {
+  console.log("listening on port " + 3001);
 });
